90e26c5dd4f18d7fe0dcb5d57b239ae9d8f41459
Giovanni ALESI <giovannialesi87@icloud.com>
2025-04-19 09:35:14 +0200

    ciclo for e ciclo while

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -3,6 +3,9 @@ package com.ferragnez.party;
 public class CheckGuest {
     public static void main(String[] args) {
         
+        int day = 1;
+        String guest;
+
         String[] guestList = {
             "Dua Lipa",
             "Paris Hilton" , 
@@ -16,11 +19,35 @@ public class CheckGuest {
             "Martina Maccherone", 
             "Rachel Zeilic"
         };
+
+        // switch (guestList) {
+        //     case 1:
+        //         guest = "Dua Lipa";
+        //         break;
+        //     case 2:
+        //         guest = "Paris Hilton";
+        //         break;
+        //     case 3:
+        //         guest = "Manuel Agnelli";
+        //         break;
         
+        //     default:  
+        //         guest= "Not found";
+        //         break;
+        // }
+        // System.out.println(guestList);
+
+
         //Ciclo for
-        for (int i=0; i < guestList.length; i++){
-            System.out.println( "to number "  + i +  "the guest is found: " + guestList[i]);
-        };
+        // for (int i=0; i < guestList.length; i++){
+        //     System.out.println( "to number "  + i +  " the guest is found: " + guestList[i]);
+        // };
+
+        //ciclo while
+        int i = 0;
+        while(i < guestList.length) {
+            System.out.println(guestList[i]);
+        }
 
     }
 }
